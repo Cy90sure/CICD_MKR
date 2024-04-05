871c0e0e5b46a8599c87f6a8c52ad18b64d9c502
Cy90sure <maria.pozniak2013@gmail.com>
2024-04-05 12:34:28 +0300

    added function for reading file

--- main.py
+++ main.py
@@ -6,7 +6,20 @@ def get_desktop_path() -> str:
     """
     return os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
 
-
+def read_file(filepath: str) -> list:
+    """
+    Зчитує вміст файлу та повертає список рядків.
+    :param filepath: Шлях до файлу.
+    :return: Список рядків у файлі.
+    """
+    try:
+        with open(filepath, 'r') as file:
+            return file.readlines()
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+    except Exception as e:
+        print("Сталася помилка під час читання файлу:", str(e))
+        return []
 
 if __name__ == "__main__":
     keyword = input("Введіть ключове слово: ")
