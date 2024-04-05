a0f94b9b8bade6ded1e95f0529217edff846ab26
Cy90sure <maria.pozniak2013@gmail.com>
2024-04-05 12:54:46 +0300

    added final test

--- tests/test_filtering.py
+++ tests/test_filtering.py
@@ -27,3 +27,22 @@ def test_filter_lines():
     ]
     filtered_lines = filter_lines(lines, "Lorem")
     assert len(filtered_lines) == 1
+
+@pytest.mark.parametrize("filtered_lines, expected_content", [
+    (["Filtered line 1", "Filtered line 2"], "Filtered line 1\nFiltered line 2"),
+    ([], "") 
+])
+
+def test_write_filtered_lines(filtered_lines, expected_content):
+    write_filtered_lines(filtered_lines)
+    desktop_path = get_desktop_path()
+    filtered_file_path = os.path.join(desktop_path, "filtered.txt")
+    if filtered_lines:
+        assert os.path.exists(filtered_file_path)  
+        with open(filtered_file_path, 'r') as file:
+            assert file.read().strip() == expected_content.strip() 
+    else:
+        if os.path.exists(filtered_file_path):
+            os.remove(filtered_file_path)  
+        assert not os.path.exists(filtered_file_path)
+        
